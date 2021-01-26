4496f7ad66f20d561c846566b3660fcc8b0ef754
ShimeX <62620132+ShimeXx@users.noreply.github.com>
2021-01-26 20:39:26 +0300

    Update shard.js

--- shard.js
+++ shard.js
@@ -2,7 +2,7 @@ const config = require('./ayarlar.json')
 const { ShardingManager } = require('discord.js');
 
 const shard = new ShardingManager('./bot.js', {
-    totalShards: 2, 
+    totalShards: 'auto', 
     token: config.token
 });
 shard.spawn();
