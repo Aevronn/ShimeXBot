357689154361cbd289c9d7602a972bc9b9ec3193
ShimeX <62620132+ShimeXx@users.noreply.github.com>
2021-03-19 17:37:56 +0300

    Update queue.js

--- komutlar/queue.js
+++ komutlar/queue.js
@@ -1,24 +1,20 @@
 const Discord = require('discord.js');
-const Player = require('discord-player');
 
 exports.run = async (client, message, args) => {
     if (!message.member.voice.channel) return message.channel.send(` You're not in a voice channel !`);
 
     if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return message.channel.send(` You are not in the same voice channel !`);
 
-    const queue = client.player.getQueue(message);
-
     if (!client.player.getQueue(message)) return message.channel.send(` No songs currently playing !`);
-
-
+   
+    let queue = client.player.getQueue(message);
     message.channel.send(new Discord.MessageEmbed()
     .setColor('RANDOM')
-    .setTitle('Server queue')
-    .setDescription(`${client.player.getQueue(message).loopMode ? '(looped)' : ''}**\nNow Playing :  ${queue.playing.title} | ${queue.playing.author}\n\n` + (queue.tracks.map((track, i) => {
-      return `**#${i + 1}** - ${track.title} | ${track.author} (requested by : ${track.requestedBy.username})`
-  }).slice(0, 5).join('\n') + `\n\n${queue.tracks.length > 5 ? `And **${queue.tracks.length - 5}** other songs...` : `In the playlist **${queue.tracks.length} song(s)...`}`))
+    .setDescription('Queue:\n'+(queue.songs.map((song, i) => {
+      return `${i === 0 ? 'Now Playing' : `#${i+1}`} - ${song.name} | ${song.author}`}).join('\n')))
     );
-  };
+
+};
 
 exports.conf = {
   enabled: true,
