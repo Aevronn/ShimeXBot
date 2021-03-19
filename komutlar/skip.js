0655965c5b420ee12c8e5863806e67c7fdc717e9
ShimeX <62620132+ShimeXx@users.noreply.github.com>
2021-03-19 17:39:17 +0300

    Update skip.js

--- komutlar/skip.js
+++ komutlar/skip.js
@@ -1,5 +1,4 @@
 const Discord = require('discord.js');
-const Player = require('discord-player');
 
 exports.run = async (client, message, args) => {
 if(!message.member.voice.channel) { return message.channel.send(new MessageEmbed().setDescription(`${client.emotes.error} | You're not in voice channel.`)) }
